9d90b053e884682d1d68faa460f668f2503ec073
Rush <kriscookies20@gmail.com>
2022-05-28 00:06:15 -0700

    for loop progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,52 @@
 
+// displays date in the header
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
+// going to tell user what hour it is
 var currentHour = moment().hour();
 console.log(currentHour)
 
 var allTextAreas = $("textarea");
-console.log(allTextAreas)
+console.log(allTextAreas);
+
+// color of teatarea
+// var color = $(this).css("background-color", "color");
+
+var textAreaTimeLoop = [
+    $("#eight-am"),
+    $("#nine-am"),
+    $("#ten-am"),
+    $("#eleven-am"),
+    $("#twelve-pm"),
+    $("#one-pm"),
+    $("#two-pm"),
+    $("#three-pm"),
+    $("#four-pm"),
+    $("#five-pm")
+];
+    var colorCoding = function() {
+    for(let i=0; i < 10; i++) {
+        if(textAreaTimeLoop.length < currentHour) {
+            $(this)
+            // $(".past").css("background-color");
+  } else {
+
+    }
+    }
+    console.log(textAreaTimeLoop.length < currentHour.length)
+
+    }
 
 
+colorCoding();
 
+// $(this).addClass("past");
 
 // var xx = new Date(Date.now());
 // var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
 // };
 
-// give each textarea an ID for its timeblock hour (military time)
-// for loop and an array for textarea
+// give each textarea an ID for its timeblock hour (military time) --
+// for loop and an array for textarea 
 // each textarea try to get idea value
-// compare id to current hour less than, past, prestn, etc.
+// compare id to current hour less than, past, prestn, etc
